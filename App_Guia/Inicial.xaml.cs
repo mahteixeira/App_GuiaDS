b8cc1772ae1fabee04b15a8b7b18b669ad244947
mahteixeira <teixeiramarina13@gmail.com>
2022-10-21 15:53:15 -0300

    carousel

--- App_Guia/Inicial.xaml.cs
+++ App_Guia/Inicial.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,29 @@ namespace App_Guia
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Inicial : ContentPage
     {
+        public class ImageInformation
+        {
+            public ImageSource _Image { get; set; }
+        }
+        private ObservableCollection<ImageInformation> imageCollection;
+
+        public ObservableCollection<ImageInformation> ImageCollection
+        {
+            get { return imageCollection; }
+            set { imageCollection = value;
+                OnPropertyChanged();    
+            }
+        }
         public Inicial()
         {
             InitializeComponent();
-            
+            BindingContext = this;
+            ImageCollection = new ObservableCollection<ImageInformation>
+            {
+                new ImageInformation{ _Image ="Imagens/foto1.png" },
+                new ImageInformation{ _Image ="Imagens/foto2.png" },
+                new ImageInformation{ _Image ="Imagens/foto3.png" }
+            };
         }
     }
 }
